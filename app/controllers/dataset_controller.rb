6555aa5202ad5bffeb12b815fdd3ef1df5167842
vkostyrka <kosvit1408@gmail.com>
2020-02-23 19:14:59 +0200

    change dashboard to dataset controller

--- app/controllers/dataset_controller.rb
+++ app/controllers/dataset_controller.rb
@@ -1,4 +1,4 @@
-class DashboardController < ApplicationController
+class DatasetController < ApplicationController
   before_action :authenticate_user!
 
   def index
---
+++ spec/controllers/dataset_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe DatasetController, type: :controller do
+end
