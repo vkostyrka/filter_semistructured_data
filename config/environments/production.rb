8003248204b19c39729ef2950b0f96e5540876c8
vkostyrka <kosvit1408@gmail.com>
2020-06-03 22:52:54 +0300

    add config for assets in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.precompile += %w( *.js *.css )
+  config.serve_static_assets = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
